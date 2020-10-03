72ab62d05ef04dd102a300a88048697d61b4f778
JohnnyBorov <JohnnyBorov@gmail.com>
2020-10-03 20:12:29 +0300

    Real data splitting.

--- QRSender/ComplexEncoder.cs
+++ QRSender/ComplexEncoder.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
 using static QRSender.HelperFunctions;
 
 namespace QRSender
@@ -15,23 +18,53 @@ namespace QRSender
 
         public async Task CreateComplex(string msg)
         {
-            int numberOfParts = 1;
+            int chunkSize = 3;
+            var dataParts = SplitStringToChunks(msg, chunkSize).ToArray();
+
+            var numberOfParts = dataParts.Length;
             var settings = numberOfParts.ToString();
 
             var settingsWritableBitmap = EncodeToQR(settings);
             this._imageSourceHolder.ImageSource = settingsWritableBitmap;
-            await Task.Delay(500);
+            await Task.Delay(250);
 
-
+            string fullData = "";
             for (int i = 0; i < numberOfParts; i++)
             {
-                string msgPart = msg;
+                string msgPart = dataParts[i];
                 var dataPartWritableBitmap = EncodeToQR(msgPart);
                 this._imageSourceHolder.ImageSource = dataPartWritableBitmap;
-                await Task.Delay(500);
+                await Task.Delay(250);
+                fullData += msgPart;
             }
+            MessageBox.Show($"Sent: {fullData}");
 
             this._imageSourceHolder.ImageSource = null;
+        }
+
+
+        private static IEnumerable<string> SplitStringToChunks(string fullStr, int chunkSize)
+        {
+            var numberOfChunks =
+                fullStr.Length % chunkSize == 0
+                ? fullStr.Length / chunkSize
+                : fullStr.Length / chunkSize + 1;
+
+
+            var chunks =
+                Enumerable.Range(0, numberOfChunks)
+                .Select(chunkNum =>
+                {
+                    int substringSize =
+                        chunkNum * chunkSize + chunkSize > fullStr.Length
+                        ? fullStr.Length % chunkSize
+                        : chunkSize;
+
+                    return fullStr.Substring(chunkNum * chunkSize, substringSize);
+                });
+
+
+            return chunks;
         }
     }
 }
--- QRSender/ComplexScanner.cs
+++ QRSender/ComplexScanner.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Windows;
 using ZXing;
 using static QRSender.HelperFunctions;
@@ -19,21 +20,31 @@ namespace QRSender
             var settingsData = settingsResult.Text;
             int numberOfParts = int.Parse(settingsData);
 
-            string fullData = "";
+
+            var receivedIDs = new HashSet<string>();
+            var settingsDataID = settingsData;
+            receivedIDs.Add(settingsDataID);
+
             int currentPart = 0;
+            var dateParts = new Dictionary<string, string>();
             while (currentPart < numberOfParts)
             {
                 var dataPartResult = await WaitForQR();
                 var currentData = dataPartResult.Text;
+                var currentDataID = currentData;
 
-                if (currentData != settingsData)
+                if (!receivedIDs.Contains(currentDataID))
                 {
+                    receivedIDs.Add(currentDataID);
+                    dateParts.Add(currentDataID, currentData);
+
                     currentPart++;
-                    fullData += currentData;
                 }
 
                 await Task.Delay(50);
             }
+
+            var fullData = string.Join("", dateParts.Values);
 
             MessageBox.Show($"Scanned: {fullData}");
         }
